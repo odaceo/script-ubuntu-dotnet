dab9cc20be22b13495600d792c2e4a9a106dd8ab
Alexandre GARINO <alexandre.garino@odaceo.ch>
2017-01-21 09:56:05 +0100

    Initial support for xenial64

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Add the .NET Core repository details
-sudo sh -c 'echo "deb [arch=amd64] http://apt-mo.trafficmanager.net/repos/dotnet/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 
 # Import the public key used by the package management system
-sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 
 # Update your local package index
 sudo apt-get update
     
 # Install .NET Core SDK
-sudo apt-get install -y dotnet
+sudo apt-get install -y dotnet-dev-1.0.0-preview2.1-003177
